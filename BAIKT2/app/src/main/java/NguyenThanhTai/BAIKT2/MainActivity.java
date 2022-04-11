0b0ae7bb61ea90b57804810eeb694d499ecac037
Leo Beo <thanhtai2902@gmail.com>
2022-04-11 20:38:12 +0700

    up

--- BAIKT2/app/src/main/java/NguyenThanhTai/BAIKT2/MainActivity.java
+++ BAIKT2/app/src/main/java/NguyenThanhTai/BAIKT2/MainActivity.java
@@ -3,9 +3,11 @@ package NguyenThanhTai.BAIKT2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText edTien;
@@ -24,10 +26,18 @@ public class MainActivity extends AppCompatActivity {
         RB13 = findViewById(R.id.RB13);
         RB13 = findViewById(R.id.RB15);
         RB13 = findViewById(R.id.RB18);
+        buttonTip = findViewById(R.id.buttonTip);
+    }
 
-
-
-
-
+    public void TinhTien(View v)
+    {
+        if(RB13.isChecked())
+            Toast.makeText(MainActivity.this,String.valueOf(Float.valueOf(edTien.getText().toString()) * 0.13) ,Toast.LENGTH_LONG).show();
+        else
+        if(RB15.isChecked())
+            Toast.makeText(MainActivity.this,String.valueOf(Float.valueOf(edTien.getText().toString()) * 0.15) ,Toast.LENGTH_LONG).show();
+        else
+        if (RB18.isChecked())
+            Toast.makeText(MainActivity.this,String.valueOf(Float.valueOf(edTien.getText().toString()) * 0.18) ,Toast.LENGTH_LONG).show();
     }
 }
