6d612821e7ee68b9f17b9da9c5f12227aed653c9
Leo Beo <thanhtai2902@gmail.com>
2022-04-11 20:32:51 +0700

    up

--- BAIKT2/app/src/main/java/NguyenThanhTai/BAIKT2/MainActivity.java
+++ BAIKT2/app/src/main/java/NguyenThanhTai/BAIKT2/MainActivity.java
@@ -3,12 +3,31 @@ package NguyenThanhTai.BAIKT2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edTien;
+    RadioButton RB13,RB15,RB18;
+    Button buttonTip;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addView(){
+        edTien = findViewById(R.id.edTien);
+        RB13 = findViewById(R.id.RB13);
+        RB13 = findViewById(R.id.RB15);
+        RB13 = findViewById(R.id.RB18);
+
+
+
+
+
     }
 }
