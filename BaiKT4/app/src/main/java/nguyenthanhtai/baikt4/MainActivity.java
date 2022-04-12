e93af3ef16931b300becfe01988568917fa060a5
Leo Beo <thanhtai2902@gmail.com>
2022-04-12 08:24:11 +0700

    up

--- BaiKT4/app/src/main/java/nguyenthanhtai/baikt4/MainActivity.java
+++ BaiKT4/app/src/main/java/nguyenthanhtai/baikt4/MainActivity.java
@@ -16,7 +16,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        List<ConVat> list = new ArrayList<ConVat>();
+        ConVat cran = new ConVat("Ga", R.drawable.chicken, " Nuoi lay thit và trung");
+        ConVat cheo = new ConVat("Pig", R.drawable.pig, "Delicious in rolls");
+        ConVat cmuc = new ConVat("Mực", R.drawable.muc, "8 tentacled monster");
+        ConVat ctho = new ConVat("Thỏ", R.drawable.tho, "nice in a stew");
+        ConVat ccuu = new ConVat("Cừu", R.drawable.cuu, "great for jumber");
+        ConVat cnhen = new ConVat("Nhện", R.drawable.nhen, "great for jumber");
+        final boolean add = list.add(cran);
+        list.add(cheo);
+        list.add(cmuc);
+        list.add(ctho);
+        list.add(ccuu);
+        list.add(cnhen);
         final ListView listView = (ListView) findViewById(R.id.listViewQG);
         listView.setAdapter(new myCustomListAdapter(list, this));
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
