8f6980292176e048ef45797454bebb83495bce89
Leo Beo <thanhtai2902@gmail.com>
2022-04-12 08:12:17 +0700

    up

--- BaiKT4/app/src/main/java/nguyenthanhtai/baikt4/MainActivity.java
+++ BaiKT4/app/src/main/java/nguyenthanhtai/baikt4/MainActivity.java
@@ -1,8 +1,14 @@
 package nguyenthanhtai.baikt4;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ListView listView = (ListView) findViewById(R.id.listViewQG);
+        listView.setAdapter(new myCustomListAdapter(list, this));
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                Object o = listView.getItemAtPosition(position);
+                ConVat animal = (ConVat) o;
+                Toast.makeText(MainActivity.this, "Selected :" + " " + animal, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
---
+++ BaiKT4/app/src/main/java/nguyenthanhtai/baikt4/myCustomListAdapter.java
@@ -0,0 +1,10 @@
+package nguyenthanhtai.baikt4;
+
+import android.widget.ListAdapter;
+
+import java.util.List;
+
+public class myCustomListAdapter implements ListAdapter {
+    public myCustomListAdapter(List<ConVat> list, MainActivity mainActivity) {
+    }
+}
